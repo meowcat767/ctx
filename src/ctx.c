f68dbc939a91741357367573db6b82815ccee1e9
Ben <meowcat@meowcat.site>
2026-07-01 17:29:04 +0100

    create a void for rawMode

--- src/ctx.c
+++ src/ctx.c
@@ -1,4 +1,22 @@
+#include <unistd.h>
+#include <termios.h>
+
+void enableRawMode()
+{
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    // Set all flags HERE!
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+}
+
 int main()
 {
+    enableRawMode();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
